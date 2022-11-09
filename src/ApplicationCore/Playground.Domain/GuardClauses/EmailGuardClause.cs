4bae30a5f4c4495164346a8ed45641a25a834f66
Georgi Georgiev <georgy.georgiev93@gmail.com>
2022-11-09 12:23:41 +0200

    save

--- src/ApplicationCore/Playground.Domain/GuardClauses/EmailGuardClause.cs
+++ src/ApplicationCore/Playground.Domain/GuardClauses/EmailGuardClause.cs
@@ -2,19 +2,20 @@
 {
     using System.Text.RegularExpressions;
 
-    internal static class EmailGuardClause
+    internal static partial class EmailGuardClause
     {
         private const string EmailRegexPattern = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
-
-        private static readonly Regex EmailRegex = new(EmailRegexPattern, RegexOptions.IgnoreCase);
 
         public static string InvalidEmail(this IGuardClause guardClause, string input, string parameterName)
         {
             Guard.Against.NullOrWhiteSpace(input, parameterName, "email is not supplied");
 
-            return EmailRegex.IsMatch(input)
+            return EmailRegex().IsMatch(input)
                 ? input
                 : throw new ArgumentException("Invalid email!", parameterName);
         }
+
+        [GeneratedRegex(EmailRegexPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled)]
+        private static partial Regex EmailRegex();
     }
 }
