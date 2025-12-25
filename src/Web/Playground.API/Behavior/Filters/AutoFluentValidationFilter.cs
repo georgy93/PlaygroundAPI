86d0a058c3865dd8489130542df50195477db6f9
Georgi Georgiev <georgy.georgiev93@gmail.com>
2025-12-25 18:45:24 +0200

    save validaitons

--- src/Web/Playground.API/Behavior/Filters/AutoFluentValidationFilter.cs
+++ src/Web/Playground.API/Behavior/Filters/AutoFluentValidationFilter.cs
@@ -8,31 +8,43 @@ public sealed class AutoFluentValidationFilter : IAsyncActionFilter
     private readonly IServiceProvider _serviceProvider;
 
     public AutoFluentValidationFilter(IServiceProvider serviceProvider)
-        => _serviceProvider = serviceProvider;
+    {
+        _serviceProvider = serviceProvider;
+    }
 
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        foreach (var (_, arg) in context.ActionArguments)
+        foreach (var (_, arg) in context.ActionArguments.Where(a => a.Value is not null))
         {
-            if (arg is null)
-                continue;
-
-            var argType = arg.GetType();
-            var validatorType = typeof(IValidator<>).MakeGenericType(argType);
-
-            var validatorObj = _serviceProvider.GetService(validatorType);
-            if (validatorObj is null)
-                continue;
-
-            var validator = (IValidator)validatorObj;
-
-            var validationContext = new ValidationContext<object>(arg);
-            var result = validator.Validate(validationContext);
-
-            if (!result.IsValid)
-                throw new ValidationException(result.Errors);
+            if (TryGetValidator(arg, out var validator))
+                ValidateAndThrow(arg, validator);
         }
 
         await next();
+    }
+
+    private bool TryGetValidator(object arg, out IValidator validator)
+    {
+        validator = null;
+
+        var argType = arg.GetType();
+        var validatorType = typeof(IValidator<>).MakeGenericType(argType);
+
+        var validatorObj = _serviceProvider.GetService(validatorType);
+        if (validatorObj is null)
+            return false;
+
+        validator = (IValidator)validatorObj;
+
+        return true;
+    }
+
+    private static void ValidateAndThrow(object arg, IValidator validator)
+    {
+        var validationContext = new ValidationContext<object>(arg);
+
+        var result = validator.Validate(validationContext);
+        if (!result.IsValid)
+            throw new ValidationException(result.Errors);
     }
 }
